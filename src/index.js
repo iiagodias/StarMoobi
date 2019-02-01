4a955cb50563292d7fea1dc8bc5f474dddb24f00
iiagodias <iago.unama@hotmail.com>
2019-02-01 17:15:09 -0200

    Configurando as rotas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import Inicio from './screens/Inicio';
+import { createStackNavigator, createAppContainer  } from 'react-navigation';
 
-export default class App extends Component {
-  render() {
-    return (
-        <Inicio />
-    );
-  }
-}
+const StackNivagator = createStackNavigator({
+    Inicio: {
+      screen: Inicio,
+      navigationOptions:{
+        header: null
+      }
+    }
+});
 
+const Route = createAppContainer(StackNivagator);
+
+export default Route;
