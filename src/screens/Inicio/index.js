94431e2e06bb3b750e697eae6c55c4f88efbb6ba
iiagodias <iago.unama@hotmail.com>
2019-02-01 22:09:27 -0200

    Requisição e tela inicial em desenvolvimento

--- src/screens/Inicio/index.js
+++ src/screens/Inicio/index.js
@@ -9,13 +9,34 @@ class Inicio extends Component {
         txtNome: ""
     }
 
+    async buscar(nome){
+        await fetch(`https://swapi.co/api/people/?search=${nome}`).then(function(response){
+             response.text().then(function(result){ 
+               result = JSON.parse(result);
+               if(result.count === 0){
+                   alert("Erro", "Nenhum personagem encontrado!");
+               }else{
+                   alert(JSON.stringify(result.results));
+               }
+             });
+        }).catch(function(error){
+            
+        });
+    }
+
     render(){
         return(
             <View style={styles.container}>
                 <View style={styles.header}>
                     <View style={styles.boxInput}>
                         <Icon name="search" size={18} color="#999" />
-                        <TextInput placeholder="Buscar" style={styles.inputBuscar} />
+                        <TextInput 
+                         placeholder="Buscar"
+                         returnKeyType="search"
+                         style={styles.inputBuscar}
+                         onChangeText={(txtNome) => this.setState({txtNome})}
+                         value={this.state.txtNome}
+                         onSubmitEditing={() => this.buscar(this.state.txtNome)} />
                     </View>
                 </View>
             </View>
